e348a42f91484a71087324e5007c7155b265bffc
marcelnoehre <marcelnoehre@web.de>
2024-12-09 19:28:48 +0100

    fix logger

--- services/logger.py
+++ services/logger.py
@@ -2,14 +2,14 @@ import datetime
 from config.log_colors import LOG_COLORS
 
 class Logger:
-    def init(self, name="System"):
+    def __init__(self, name):
         self.name = name
 
     def _current_time(self):
         return datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
 
     def _log(self, msg, color, level):
-        print(f"{LOG_COLORS["green"]}{self._current_time()}{LOG_COLORS["white"]} | {LOG_COLORS[color]}{level}{LOG_COLORS["white"]} | {LOG_COLORS["purple"]}[{self.name}] {LOG_COLORS["reset"]}{msg}")
+        print(f"{LOG_COLORS['green']}{self._current_time()}{LOG_COLORS['white']} | {LOG_COLORS[color]}{level}{LOG_COLORS['white']} | {LOG_COLORS['purple']}[{self.name}] {LOG_COLORS['reset']}{msg}")
 
     def info(self, msg):
         self._log(msg, "cyan", "INFO   ")
