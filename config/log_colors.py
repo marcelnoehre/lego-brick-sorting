245f4847383b5ebcf008c87bcb998561e75828c3
marcelnoehre <marcelnoehre@web.de>
2024-12-09 19:18:02 +0100

    log colors

---
+++ config/log_colors.py
@@ -0,0 +1,11 @@
+LOG_COLORS = {
+    "reset": "\x1b[0m",
+    "red": "\x1b[31;20m",
+    "green": "\x1b[32;20m",
+    "yellow": "\x1b[33;20m",
+    "blue": "\x1b[34;20m",
+    "purple": "\x1b[35;20m",
+    "cyan": "\x1b[36;20m",
+    "white": "\x1b[37;20m",
+    "bold_underlined_purple": "\x1b[35;1;4m",
+}
