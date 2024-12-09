847ad8b1e2f74b0215c806c055c008f9784fbe5b
marcelnoehre <marcelnoehre@web.de>
2024-12-09 19:36:49 +0100

    start/stop machine thread

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import threading
 from config.log_colors import LOG_COLORS
 from services.logger import Logger
+from components.machine import Machine
 
 def print_commands():
     print(f"""{LOG_COLORS["bold_underlined_purple"]}Lego Brick Sorting Machine!{LOG_COLORS["reset"]}""")
     print(f"* {LOG_COLORS['blue']}start{LOG_COLORS['reset']} - Starts the sorting machine")
     print(f"* {LOG_COLORS['blue']}stop{LOG_COLORS['reset']} - Stops the sorting machine")
     print(f"* {LOG_COLORS['blue']}quit{LOG_COLORS['reset']} - Quits the program")
+
 
 def user_input():
     global machine_thread
@@ -15,23 +18,24 @@ def user_input():
     
     while True:
         command = input("").strip().lower()
-
         if command == "start":
             if not is_running:
                 logger.info("Starting the sorting machine")
-                # start machine
+                machine = Machine()
+                machine_thread = threading.Thread(target=machine.start, daemon=True)
+                machine_thread.start()
                 is_running = True
             else:
                 logger.warning("The sorting machine is already running!")
-        
+
         elif command == "stop":
             if is_running:
                 logger.info("Stopping the sorting machine")
-                # stop machine
+                machine.stop()
                 is_running = False
             else:
                 logger.warning("The sorting machine is not running!")
-        
+
         elif command == "quit":
             logger.info("Quitting the program")
             exit()
@@ -39,6 +43,7 @@ def user_input():
         else:
             logger.warning("Invalid command! Please use one of the following commands:")
             print_commands()
+
 
 if __name__ == "__main__":
     print_commands()
