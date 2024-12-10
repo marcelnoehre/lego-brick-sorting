e064057b07646e25406f7f1b53745d23d5e3cecd
marcelnoehre <marcelnoehre@web.de>
2024-12-11 00:28:29 +0100

    iterative checks of sensor state

--- components/light_barrier.py
+++ components/light_barrier.py
@@ -22,4 +22,31 @@ class LightBarrier:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.IN)
 
-    
+    def _check_sensor_state(self):
+        """
+        Checks the current state of the sensor and emits an event if the state has changed.
+        """
+        value = GPIO.input(self.pin)
+        if value != self._value:
+            self._value = value
+            self._emit_event(value)
+
+    def _emit_event(self, value):
+        """
+        Emits an event and triggers all subscribed callbacks.
+
+        :param value: The new sensor value (0 for interruption, 1 for light)
+        """
+        for callback in self._callbacks:
+            callback(value)
+
+    def _monitor(self):
+        """
+        Starts monitoring the sensor and checks for changes in a separate thread.
+        """
+        try:
+            while self._monitoring_thread is not None:
+                self._check_sensor_state()
+                time.sleep(self.interval)
+        finally:
+            GPIO.cleanup()
