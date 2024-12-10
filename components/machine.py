569b2386bef0be430561c3b18b2014bf6672d912
marcelnoehre <marcelnoehre@web.de>
2024-12-10 22:42:00 +0100

    handle logger as class param

--- components/machine.py
+++ components/machine.py
@@ -3,18 +3,20 @@ from services.logger import Logger
 
 class Machine:
     def __init__(self):
+        self.logger = Logger("Machine")
         self.is_running = False
 
     def start(self, *args, **kwargs):
-        logger = Logger("Machine")
         self.is_running = True
+        self.logger.info("Machine started")
         while self.is_running:
-            logger.info("Sorting bricks")
+            self.logger.info("Sorting bricks")
             time.sleep(3)
-            logger.warning("Sorting bricks")
+            self.logger.warning("Sorting bricks")
             time.sleep(3)
-            logger.error("Sorting bricks")
+            self.logger.error("Sorting bricks")
             time.sleep(3)
 
     def stop(self, *args, **kwargs):
         self.is_running = False
+        self.logger.info("Machine stopped")
